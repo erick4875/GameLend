8f09b04df830a8db49deff36d3ba7a619a161020
Abarnunn <adriigali@gmail.com>
2025-04-24 13:26:14 +0200

    editarPerfil.xml

--- app/src/main/java/com/example/gamelend/Activities/InfoUsuario.java
+++ app/src/main/java/com/example/gamelend/Activities/InfoUsuario.java
@@ -1,6 +1,31 @@
 package com.example.gamelend.Activities;
 
 import android.app.Activity;
+import android.os.Bundle;
 
-public class InfoUsuario extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.gamelend.R;
+
+public class InfoUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_usuario);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.recyclerView), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
 }
